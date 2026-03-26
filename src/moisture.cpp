d04aad965774207edc4f3bab78d501753c38e13b
Katherine717 <katy717nd@gmail.com>
2026-03-26 11:03:22 -0700

    change pin #

--- src/moisture.cpp
+++ src/moisture.cpp
@@ -1,12 +1,9 @@
-#define soil_moisture_pin 10
-
+#define soil_moisture_pin 4
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int soil_moisture_value = analogRead(soil_moisture_pin);
-  Serial.print("Soil Moisture Value: ");
-  Serial.println(soil_moisture_value);
-  delay(1000); // Delay for a second before the next reading
+  Serial.printIn(analogRead(soil_moisture_pin));
+  delay(500);
 }
